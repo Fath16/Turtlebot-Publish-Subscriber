a505813036d68d7116d0b3fe04d6a170163c3086
Fath16 <cffath16@gmail.com>
2024-03-06 03:04:47 -0500

    Your commit message here

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/theanvin/Workspaces/smb_ws/src
+# Build directory: /home/theanvin/Workspaces/smb_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("teleop_twist_keyboard")
+subdirs("smb_control")
+subdirs("smb_gazebo")
+subdirs("smb_description")
