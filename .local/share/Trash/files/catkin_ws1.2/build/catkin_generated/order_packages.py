695a7430492079dcc456b64c45e0aa2819dba941
Fath16 <106072698+Fath16@users.noreply.github.com>
2024-03-19 13:51:30 -0400

    Delete .local directory

--- .local/share/Trash/files/catkin_ws1.2/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/theanvin/catkin_ws1/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/theanvin/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/theanvin/catkin_ws/devel;/opt/ros/noetic' != '' else []
--- .local/share/Trash/files/catkin_ws1.2/build/smb_common/teleop_twist_keyboard/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/theanvin/catkin_ws1/src/smb_common/teleop_twist_keyboard
-# Build directory: /home/theanvin/catkin_ws1/build/smb_common/teleop_twist_keyboard
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
