d4585f0b3546461e53b1114db860d88ac7afef15
Fath16 <cffath16@gmail.com>
2024-03-28 04:34:10 -0400

    Remove all files and directories except submodules

--- build/smb_common/smb_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/theanvin/Robotic_class/ws/src/smb_common/smb_description
-# Build directory: /home/theanvin/Robotic_class/ws/build/smb_common/smb_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/smb_common/teleop_twist_keyboard/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/theanvin/Robotic_class/ws/src/smb_common/teleop_twist_keyboard
-# Build directory: /home/theanvin/Robotic_class/ws/build/smb_common/teleop_twist_keyboard
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
