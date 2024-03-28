09b643b6e076eeac4aa0dcf50afc15e50bfefbfa
Fath16 <cffath16@gmail.com>
2024-03-28 12:39:12 -0400

    hw5

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/theanvin/ws3/src
+# Build directory: /home/theanvin/ws3/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("twolink_description")
---
+++ build/twolink_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/theanvin/ws3/src/twolink_description
+# Build directory: /home/theanvin/ws3/build/twolink_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
